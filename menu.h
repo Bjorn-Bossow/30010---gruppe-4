323616c865576b4a126443251c60e2c4e0ab76fb
PatrickPatte <s245777@dtu.dk>
2025-01-19 16:22:19 +0100

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,16 @@
+/*
+ * menu.h
+ *
+ *  Created on: 16 Jan 2025
+ *      Author: pat99
+ */
+
+#ifndef MENU_H_
+#define MENU_H_
+
+void menu();
+void difficulty_select();
+void help_screen();
+
+
+#endif /* MENU_H_ */
